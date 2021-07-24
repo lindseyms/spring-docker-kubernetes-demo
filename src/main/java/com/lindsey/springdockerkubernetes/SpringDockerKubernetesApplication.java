db7fd90cbd1773a39e782b2c4876fe9ab22b040d
lindseyms <51429410+lindseyms@users.noreply.github.com>
2021-07-24 14:58:01 -0500

    Initial commit

---
+++ src/main/java/com/lindsey/springdockerkubernetes/SpringDockerKubernetesApplication.java
@@ -0,0 +1,13 @@
+package com.lindsey.springdockerkubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerKubernetesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDockerKubernetesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lindsey/springdockerkubernetes/SpringDockerKubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lindsey.springdockerkubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerKubernetesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
